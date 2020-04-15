b32a54217d40c808ac437303be303e7ef3444fb1
Daniel <chesterdany22@yahoo.com>
2020-04-15 20:58:38 +0300

    major structure implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBilder/BurgerBuilder";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Aux from "../../hoc/Auxiliar";
+import classes from "./Layout.modules.css";
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
